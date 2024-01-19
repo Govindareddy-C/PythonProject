9e02f314d22e383437e26f1442ac9eac08fcb1e8
Govind <govindareddy9704@gmail.com>
2024-01-19 20:43:42 +0530

    Players maximum score increased to 25(But score is not calculating individually

--- PythonProjects/Piggame.py
+++ PythonProjects/Piggame.py
@@ -12,19 +12,19 @@ def roll_out():
 
 # if player_score < 25:
 # means the below code has to be exicute for all three players until the player_score get 25 or more than 25
-
-for i in players_list:
-    print("Player ", i, "turn")
-    user_input = input("You want to roll it(y/n): ")
-    if user_input == 'y':
-        roll_out()
-        if random_number == 6:
-            player_score = player_score + random_number
-            print("play again")
+while player_score <= 25:
+    for i in players_list:
+        print("Player ", i, "turn")
+        user_input = input("You want to roll it(y/n): ")
+        if user_input == 'y':
             roll_out()
-        else:
-            player_score = player_score + random_number
-            print("Total_score = ", player_score)
+            if random_number == 6:
+                player_score = player_score + random_number
+                print("play again")
+                roll_out()
+            else:
+                player_score = player_score + random_number
+                print("Total_score = ", player_score)
 
 
 
